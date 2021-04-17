8a63d0a1371da999e56f09fbbe59e3711d07feb7
10-disha <jayswaldisha5@gmail.com>
2021-04-17 10:01:40 +0530

    completed

---
+++ index.js
@@ -0,0 +1,45 @@
+require('dotenv').config();
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+
+
+const homeFile = fs.readFileSync("home.html", "utf-8");
+
+
+const replaceVal = (tempVal, orgVal) => {
+    let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+    temperature = temperature.replace("{%location%}", orgVal.name);
+    temperature = temperature.replace("{%country%}", orgVal.sys.country);
+    temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+
+    return temperature;
+};
+
+const server = http.createServer((req, res) => {
+    if (req.url == "/") {
+        requests(
+                `http://api.openweathermap.org/data/2.5/weather?q=indore&units=metric&appid=b0d161fd14a2913785aacf88a9a33d15`
+            )
+            .on("data", (chunk) => {
+                const objdata = JSON.parse(chunk);
+                const arrData = [objdata];
+                // console.log(arrData[0].main.temp);
+                const realTimeData = arrData
+                    .map((val) => replaceVal(homeFile, val))
+                    .join("");
+                res.write(realTimeData);
+                // console.log(realTimeData);
+            })
+            .on("end", (err) => {
+                if (err) return console.log("connection closed due to errors", err);
+                res.end();
+            });
+    } else {
+        res.end("File not found");
+    }
+});
+
+server.listen(80, "127.0.0.1");
